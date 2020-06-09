0b855c14232b61a6606a7999a63d7a7910820955
David Bae <baeddavid@gmail.com>
2020-06-09 18:54:29 -0500

    add no data structure solution

--- ctci/UniqueCharacter.java
+++ ctci/UniqueCharacter.java
@@ -11,4 +11,17 @@ class UniqueCharacter {
 		}
 		return true;
 	}
+
+	// Solution without using extra data structure
+	boolean uniqueCharacterTime(String string) {
+		for(int i = 0; i < string.length; i++) {
+			for(int j = i + 1; j < string.length; j++) {
+				if(string.charAt(i) == string.charAt(j))
+					return false;
+			}
+		}
+		return true;
+	}
+
+	
 }
