92e3898cc96dbc59392bb92105df87ee902a288d
David Bae <baeddavid@gmail.com>
2020-06-10 12:53:17 -0500

    add solution that uses ascii array cache to achieve linear time and constant space

--- ctci/IsStringPermutation.java
+++ ctci/IsStringPermutation.java
@@ -11,5 +11,28 @@ public class IsStringPermutation {
 
 		return Arrays.equals(charS1, charS2);	
 	}
-	
+
+	// Creates an additional data structure to cache the ASCII code. Linear time and constant space soltuion.
+	boolean isStringPermutationCache(String s1, String s2) {
+		// Convert both strings to lowercase, since it was not specified.
+		s1 = s1.toLowerCase();
+		s2 = s2.toLowerCase();
+
+		int[] ascii = new int[128];
+
+		for(int i = 0; i < s1.length(); i++) {
+			ascii[s1.charAt(i)]++;
+		}
+
+		for(int j = 0; j < s2.length(); j++) {
+			ascii[s2.charAt(j)]--;
+		}
+
+		for(int c : ascii) {
+			if(c != 0)
+				return false;
+		}
+
+		return true;
+	}
 }
