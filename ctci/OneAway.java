50f05a22e50ad89760f3a7ce7aef433384ab5974
David Bae <baeddavid@gmail.com>
2020-06-11 13:25:31 -0500

    add solution for one edit away

--- ctci/OneAway.java
+++ ctci/OneAway.java
@@ -1,12 +1,37 @@
 public class OneAway {
-	boolean oneAwayCheck(String str1, String str2) {
-		if(Math.abs(str1.length() - str2.length()) > 1) return false;
-		int count = 0;
-		for(int i = 0; i < str1.length(); i++) {
-			if(!str1.charAt(i).equals(str2.chartAt(i)))
-				count++;
-			if(count > 1)
-				return false;
+	boolean oneEditAway(String first, String second) {
+		if(first.length() == second.length())
+			return oneEditReplace(first, second);
+		else if(first.length() + 1 == second.length())
+			return oneEditInsert(first, second);
+		else if(first.length() - 1 == second.length())
+			return oneEditInsert(second, first);
+	}
+
+	boolean oneEditReplace(String s1, String s2) {
+		boolean difference = false;
+		for(int i = 0; i < s1.length(); i++) {
+			if(s1.charAt(i) != s2.charAt(i)) {
+				if(difference)
+					return false;
+				difference = true;
+			}
+		}
+		return true;
+	}
+
+	boolean oneEditInsert(String s1, String s2) {
+		int index1 = 0;
+		int index2 = 0;
+		while(index2 < s2.length() && index1 < s1.length()) {
+			if(s1.charAt(index1) != s2.charAt(index2)) {
+				if(index1 != index2)
+					return false;
+				index2++;
+			} else {
+				index1++;
+				index2++;
+			}
 		}
 		return true;
 	}
