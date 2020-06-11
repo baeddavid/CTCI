b2f040ab29ddfc207916b22828ff68263afdaa66
David Bae <baeddavid@gmail.com>
2020-06-11 13:40:48 -0500

    add solution for oneEditAway with condensed solution

--- ctci/OneAway.java
+++ ctci/OneAway.java
@@ -35,4 +35,28 @@ public class OneAway {
 		}
 		return true;
 	}
+
+	boolean oneEditAwayCompress(String first, String second) {
+		if(Math.abs(first.length() - second.length()) > 1) return false;
+
+		String s1 = first.length() < second.length() ? first : second;
+		String s2 = first.length() < second.length() ? second : first;
+
+		int index1 = 0, index2 = 0;
+		boolean foundDifference = false;
+		while(index1 < s1.length() && index2 < s2.length()) {
+			if(s1.charAt(index1) != s2.charAt(index2)) {
+				if(foundDifference)
+					return false;
+				foundDifference = true;
+
+				if(s1.length() == s2.length())
+					index1++;
+			} else {
+				index1++;
+			} 
+			index2++;
+		}
+		return true;
+	}
 }
