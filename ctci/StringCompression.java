3a621cddf20d62909b900a13a9edd9a713411067
David Bae <baeddavid@gmail.com>
2020-06-11 16:37:24 -0500

    cleaned up code and optimized for performance

--- ctci/StringCompression.java
+++ ctci/StringCompression.java
@@ -1,21 +1,24 @@
 public class StringCompression {
 	String compressString(String str) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("");
+
 		int count = 1;
 		char key = str.charAt(0);
-        String ans = "";
 		for(int i = 1; i < str.length(); i++) {
             System.out.println(key);
 			if(str.charAt(i) == key)
 				count++;
 			else {
-				ans += String.valueOf(key);
-				ans += String.valueOf(count);
+				sb.append(String.valueOf(key));
+				sb.append(String.valueOf(count));
 				key = str.charAt(i);
 				count = 1;
 			}
 		}
-        ans += String.valueOf(key);
-		ans += String.valueOf(count);
+		sb.append(String.valueOf(key));
+		sb.append(String.valueOf(count));
+        String ans = sb.toString();
         
 		return str.length() < ans.length() ? str : ans;
 	}
