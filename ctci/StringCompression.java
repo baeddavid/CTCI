092a7dc245c3ceffaf36e90478a28df1b6502342
David Bae <baeddavid@gmail.com>
2020-06-13 09:32:52 -0500

    add optimized solution

--- ctci/StringCompression.java
+++ ctci/StringCompression.java
@@ -23,4 +23,35 @@ public class StringCompression {
         
 		return str.length() < ans.length() ? str : ans;
 	}
+
+	// Optimized Solution
+	String compressStringOptimize(String str) {
+		int finalLength = countCompression(str);
+		if(finalLength >= str.length()) return str;
+
+		StringBuilder compressed = new StringBuilder(finalLength);
+		int countConsecutive = 0;
+		for(int i = 0; i < str.length(); i++) {
+			countConsecutive++;
+			if(i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+				compressed.append(str.charAt(i));
+				compressed.append(countConsecutive);
+				countConsecutive = 0;
+			}
+		}
+		return compressed.toString();
+	}
+
+	int countCompression(String str) {
+		int compressedLength = 0;
+		int countConsecutive = 0;
+		for(int i = 0; i < str.length(); i++) {
+			countConsecutive++;
+			if(i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+				compressedLength += 1 + String.valueOf(countConsecutive).length();
+				countConsecutive = 0;
+			}
+		}
+		return compressedLength;
+	}
 }
