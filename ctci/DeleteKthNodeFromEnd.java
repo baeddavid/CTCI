5755504048205ea27c619eaa4857971e71bd2ea3
David Bae <baeddavid@gmail.com>
2020-06-18 11:30:29 -0500

    add solution for deleting kth node from end of linked list in java

---
+++ ctci/DeleteKthNodeFromEnd.java
@@ -0,0 +1,29 @@
+public class DeleteKthNodeFromEnd {
+    public Node <Integer> deleteKthNode(Node <Integer> head, int k) {
+		Node <Integer> slow = head, fast = head;
+		while(fast != null && k != 0) {
+			fast = fast.next;
+            k--;
+		}
+
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+    
+            
+		Node <Integer> delete = slow.next;
+		slow.next = slow.next.next;
+		return delete;
+	}
+}
+
+class Node <T> {
+	public T data;
+	public Node next;
+
+	public Node(T data) {
+		this.data = data;
+		next = null;
+	}
+}
