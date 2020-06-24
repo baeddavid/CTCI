e711f2e7b2d6f73a7c8cc587eecb1bd25f45cd0e
David Bae <baeddavid@gmail.com>
2020-06-24 18:09:24 -0500

    add optimal solution for applying permutation

--- ctci/lc/ApplyPermutation.java
+++ ctci/lc/ApplyPermutation.java
@@ -10,11 +10,28 @@ public class ApplyPermutation {
         return B;
     }
 
+    // Optimized Solution
+    public static int[] applyPermutation(int[] A, int[] P) {
+        for(int i = 0; i < P.length; i++) {
+            if(i != P[i]) {
+                swap(A, i, P[i]);
+                swap(P, i, P[i]);
+            }
+        }
+        return A;
+    }
+
+    private static void swap(int[] A, int a, int b) {
+        int temp = A[a];
+        A[a] = A[b];
+        A[b] = temp;
+    }
+
     public static void main(String[] args) {
         int[] A = new int[] {1, 2, 3, 4};
         int[] P = new int[] {2, 0, 1, 3};
 
-        int[] C = applyPermutationLinSpace(A, P);
+        int[] C = applyPermutation(A, P);
         for(int x : C)
             System.out.print(x + " ");
     }
