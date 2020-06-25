de4a0f67f2ba92571476e050c3ccd5f071ce9690
David Bae <baeddavid@gmail.com>
2020-06-25 01:54:24 -0500

    add brute force solution

--- ctci/lc/ArrayLeaders.java
+++ ctci/lc/ArrayLeaders.java
@@ -16,4 +16,19 @@ public class ArrayLeaders {
 
         return leaders;
     }
+
+    public static List <Integer> arrayLeadersBruteforce(List <Integer> A) {
+        List <Integer> leaders = new ArrayList <>();
+        for(int i = 0; i < A.size(); i++) {
+            for(int j = i + 1; j < A.size(); j++) {
+                if(A[j] >= A[i]) {
+                    break;
+                }
+
+                if(j == A.size()) {
+                    leaders.add(A.get(j));
+                }
+            }
+        }
+    }
 }
