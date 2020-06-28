dc820a78ed1bd41fb6c2c8df6ca861593669181f
David Bae <baeddavid@gmail.com>
2020-06-27 21:02:01 -0500

    add solution for hashmap solution

--- ctci/lc/ValidAnagram.java
+++ ctci/lc/ValidAnagram.java
@@ -1,11 +1,41 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ValidAnagram {
     public boolean validAnagramSort(String s, String t) {
-       char[] S = s.toCharArray();
-       char[] T = t.toCharArray();
-       Arrays.sort(S);
-       Arrays.sort(T);
-       return Arrays.equals(S, T);
+        char[] S = s.toCharArray();
+        char[] T = t.toCharArray();
+        Arrays.sort(S);
+        Arrays.sort(T);
+        return Arrays.equals(S, T);
     }
+
+    public boolean validAnagramMap(String s, String t) {
+        if(s.length() != t.length()) return false;
+        HashMap <Character, Integer> M = new HashMap <>();
+        for(int i = 0; i < s.length(); i++) {
+            if(M.containsKey(s.charAt(i)))
+                M.put(s.charAt(i), M.get(s.charAt(i)) + 1);
+            else
+                M.put(s.charAt(i), 1);
+        }
+
+        for(int j = 0; j < t.length(); j++) {
+            if(M.containsKey(t.charAt(j))) {
+                M.put(t.charAt(j), M.get(t.charAt(j)) - 1);
+                if(M.get(t.charAt(j)) < 0)
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        for(int k = 0; j < t.length(); k++) {
+            if(M.get(t.charAt(k) != 0)
+               return false;
+               }
+            return true;
+        }
+    }
+
 }
