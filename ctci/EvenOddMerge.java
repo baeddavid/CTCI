df0e6a4218d958ed83977e478a5c8b38bbc9bd30
David Bae <baeddavid@gmail.com>
2020-07-09 01:11:56 -0500

    fixed formatting

--- ctci/EvenOddMerge.java
+++ ctci/EvenOddMerge.java
@@ -1,59 +1,59 @@
 public class EvenOddMerge {
     // Brute Force Solution
     public static Node <Intger> evenOddMergeBrute(Node <Integer> head) {
-	Node <Integer> newHead = new Node <>(-1);
-	Node <Integer> curr = head, newCurr = newHead;
+        Node <Integer> newHead = new Node <>(-1);
+        Node <Integer> curr = head, newCurr = newHead;
 
-	while(curr != null) {
-	    if(curr.data % 2 == 0) {
-		newCurr.next = new Node <Integer> (curr.data);
-		newCurr = newCurr.next;
-	    } else {
-		newCurr.next = new Node <Integer> (curr.data);
-		newCurr = newCurr.next;
-	    }
-	}
-	return newHead.next;
+        while(curr != null) {
+            if(curr.data % 2 == 0) {
+                newCurr.next = new Node <Integer> (curr.data);
+                newCurr = newCurr.next;
+            } else {
+                newCurr.next = new Node <Integer> (curr.data);
+                newCurr = newCurr.next;
+            }
+        }
+        return newHead.next;
     }
     
     // Optimal Solution
     public static Node <Integer> evenOddMerge(Node <Integer> head) {
-	Node <Integer> evenHead = new Node <Integer> (-1);
-	Node <Integer> oddHead = new Node <Integer> (-1);
-	Node <Integer> curr = head, evenCurr = evenHead, oddCurr = oddHead;
+        Node <Integer> evenHead = new Node <Integer> (-1);
+        Node <Integer> oddHead = new Node <Integer> (-1);
+        Node <Integer> curr = head, evenCurr = evenHead, oddCurr = oddHead;
 
-	while(curr != null) {
-	    Node <Integer> next = curr.next;
-	    if(curr.data % 2 == 0) {
-		evenCurr.next = curr;
-		evenCurr = evenCurr.next;
-	    } else {
-		oddCurr.next = curr;
-		oddCurr = oddCurr.next;
-	    }
-	    curr.next = null;
-	    curr = next;
-	}
-	evenCurr.next = oddHead.next;
-	return evenHead.next;
+        while(curr != null) {
+            Node <Integer> next = curr.next;
+            if(curr.data % 2 == 0) {
+                evenCurr.next = curr;
+                evenCurr = evenCurr.next;
+            } else {
+                oddCurr.next = curr;
+                oddCurr = oddCurr.next;
+            }
+            curr.next = null;
+            curr = next;
+        }
+        evenCurr.next = oddHead.next;
+        return evenHead.next;
     }
 
     public static void main(String[] args) {
-	Node <Integer> a = new Node <>(2);
-	Node <Integer> b = new Node <>(5);
-	Node <Integer> c = new Node <>(4);
-	Node <Integer> d = new Node <>(7);
+        Node <Integer> a = new Node <>(2);
+        Node <Integer> b = new Node <>(5);
+        Node <Integer> c = new Node <>(4);
+        Node <Integer> d = new Node <>(7);
 
-	a.next = b;
-	b.next = c;
-	c.next = d;
+        a.next = b;
+        b.next = c;
+        c.next = d;
 
-	Node <Integer> newHead = evenOddMerge(a);
-	Node <Integer> curr = newHead;
-	while(curr != null) {
-	    System.out.print(curr.data + " ");
-	    curr = curr.next;
-	}
+        Node <Integer> newHead = evenOddMerge(a);
+        Node <Integer> curr = newHead;
+        while(curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
     }
 }
 
@@ -62,8 +62,8 @@ class Node <T> {
     public Node next;
 
     public Node(T data) {
-	this.data = data;
-	next = null;
+        this.data = data;
+        next = null;
     }
 }
 
