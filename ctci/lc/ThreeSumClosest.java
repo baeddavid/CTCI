3da7a684073aa71f754d729d143e1cbaa9c12991
David Bae <baeddavid@gmail.com>
2020-07-25 17:31:54 -0500

    add solution for three sum closest"

--- ctci/lc/ThreeSumClosest.java
+++ ctci/lc/ThreeSumClosest.java
@@ -1,25 +1,27 @@
 import java.util.Arrays;
 
 public class ThreeSumClosest {
-    int threeSumClosest(int[] A, int k) {
-        Arrays.sort(A);
-        int currentSum = A[0] + A[1] + A[A.length - 1];
-        int currentMin = Math.abs(currentSum - k);
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int currentSum = nums[0] + nums[1] + nums[nums.length - 1];
+        int currentMin = Math.abs(currentSum - target);
 
-        for(int i = 0; i < A.length - 2; i++) {
-            int p1 = i + 1, p2 = A.length - 1;
+        for(int i = 0; i < nums.length - 2; i++) {
+            int p1 = i + 1, p2 = nums.length - 1;
             while(p1 < p2) {
-                int sum = A[i] + A[p1] + A[p2];
-                if(sum < k) {
+                int sum = nums[i] + nums[p1] + nums[p2];
+
+                if(Math.abs(sum - target) < currentMin) {
+                    currentMin = Math.abs(sum - target);
+                    currentSum = sum;
+                }
+                if(sum < target) {
                     p1++;
                 } else {
-                    p2++;
+                    p2--;
                 }
             }
-            if(Math.abs(sum - v) < currentMin) {
-                currentMin = Math.abs(sum - v);
-            }
         }
-        return currentMin;
+        return currentSum;
     }
 }
