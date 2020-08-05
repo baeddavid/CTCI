9ea4fe2789ffdc8639c461adc8e8057940114370
David Bae <baeddavid@gmail.com>
2020-08-05 17:55:31 -0500

    add recursive solution

--- ctci/lc/SearchBinarySearchTree.java
+++ ctci/lc/SearchBinarySearchTree.java
@@ -2,13 +2,11 @@ public class SearchBinarySearchTree {
     // Recursive
     public TreeNode searchBST(TreeNode root, int val) {
         if(root != null) {
-            if(root.data == val) {
+            if(root.val == val) {
                 return root;
-            } else if(val > root.data) {
-                searchBST(root.right, val);
-            } else {
-                searchBST(root.left, val);
-            }
+            } else if(val < root.val) {
+                return searchBST(root.left, val);
+            } return searchBST(root.right, val);
         }
         return null;
     }
