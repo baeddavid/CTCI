00a87a6cc75269a596777ad701c3106c91ae7318
David Bae <baeddavid@gmail.com>
2020-08-15 16:56:26 -0500

    add solution for merging trees in a balanced output

--- ctci/lc/CombineTwoTrees.java
+++ ctci/lc/CombineTwoTrees.java
@@ -31,4 +31,60 @@ public class CombineTwoTrees {
             prev.right = insert;
         }
     }
+
+    // Balanced Tree Solution
+    public TreeNode mergeTreesBalanced(TreeNode rootA, TreeNode rootB) {
+        List<Integer> L1 = new ArrayList<>();
+        List<Integer> L2 = new ArrayList<>();
+
+        addValuestoList(rootA, L1);
+        addValuestoList(rootB, L2);
+
+        List<Integer> mergedList = mergeLists(L1, L2);
+        TreeNode root = buildTree(mergedList, 0, mergedList.size() - 1);
+        return root;
+    }
+
+    private TreeNode buildTree(List<Integer> list, int left, int right) {
+        if(left > right) {
+            return null;
+        }
+
+        int med = left + (right - left) / 2;
+        TreeNode root = list.get(med);
+        root.left = buildTree(list, left, med - 1);
+        root.right = buildTree(list, med + 1, right);
+
+        return root;
+    }
+
+    private List<Integer> mergeLists(List<Integer> L1, List<Integer> L2) {
+        List<Integer> mergeList= new ArrayList<>();
+        int pointerL1 = 0, pointerL2 = 0;
+
+        while(pointerL1 < L1.size() && pointerL2 < L2.size()) {
+            if(L1.get(pointerL1) < L2.get(pointerL2)) {
+                mergeList.add(L1.get(pointerL1++));
+            } else {
+                mergeList.add(L2.get(pointerL2++));
+            }
+        }
+
+        while(pointerL1 < L1.size()) {
+            mergeList.add(L1.get(pointerL1++));
+        }
+
+        while(pointerL2 < L2.size()) {
+            mergeList.add(L2.get(pointerL2++));
+        }
+        return mergeList;
+    }
+
+    private void addValuestoList(TreeNode root, List<Integer> L) {
+        if(root != null) {
+            addValuestoList(root.left, L);
+            L.add(root.val);
+            addValuestoList(root.right, L);
+        }
+    }
 }
