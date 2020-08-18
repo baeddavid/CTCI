2b58b8ec39036aad54c6a03cdcd47099597c6088
David Bae <baeddavid@gmail.com>
2020-08-18 14:28:37 -0500

    add solution for getting the diameter of a binary tree

---
+++ ctci/lc/DiamaterBinaryTree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+
+    private int depth(TreeNode root) {
+        if(root == null) return 0;
+        int L = depth(root.left);
+        int R = depth(root.right);
+        ans = Math.max(ans, L + R + 1);
+        return Math.max(L, R) + 1;
+    }
+}
