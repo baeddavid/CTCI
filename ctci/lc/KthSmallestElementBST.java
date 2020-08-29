91beff1c0ad284730191f2438474eb2a47f05fdf
David Bae <baeddavid@gmail.com>
2020-08-29 17:47:15 -0500

    add stack iterative solution and a clever solution that takes advantage of finding the size of subtrees

--- ctci/lc/KthSmallestElementBST.java
+++ ctci/lc/KthSmallestElementBST.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Recursive Solution
     public int kthSmallesty(TreeNode root, int k) {
         List<Integer> inorder = new ArrayList<>();
         dfsArray(root, inorder);
@@ -11,5 +12,39 @@ class Solution {
             inorder.add(root.val);
             dfsArray(root.right, k, inorder);
         }
+    }
+
+    // Iterative Stack Solution
+    public int KthSmallest(TreeNode root, int k) {
+        Stack<Integer> stack = new Stack<>();
+        while(!stack.isEmpty() || root != null) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode current = stack.pop();
+            k--;
+            if(k == 0) return current.val;
+            current = current.right;
+        }
+        // This should be unreachable if the inputs are valid
+        return -1;
+    }
+
+    // Clever approach
+    public int kthSmallest(TreeNode root, int k) {
+        if(root == null) return null;
+        TreeNode current = root;
+        while(current != null) {
+            int leftTreeSize = current.left;
+            if(leftTreeSize + 1 > k) {
+                k -= (leftTreeSize + 1);
+                current = current.right;
+            } else if(leftTreeSize == k - 1) return current.val;
+            else {
+                current = current.left;
+            }
+        }
+        return null;
     }
 }
