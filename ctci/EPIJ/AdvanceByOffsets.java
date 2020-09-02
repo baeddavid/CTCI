868b1c010d60f128af7bed8b4647246003265a44
David Bae <baeddavid@gmail.com>
2020-09-02 13:33:46 -0500

    add solution for advance through an array

--- ctci/EPIJ/AdvanceByOffsets.java
+++ ctci/EPIJ/AdvanceByOffsets.java
@@ -1,9 +1,12 @@
-public class AdvanceByOffsets {
-    public static boolean advanceByOffsets(int[] A) {
-	int furthestReachSoFar = 0, lastIndex = A.length - 1;
-	for(int i = 0; i <= furthestReachSoFar && furthestReachSoFar < lastIndex; i++) {
-	    furthestReachSoFar = Math.max(furthestReachSoFar, i + A[i]);
-	}
-	return furthestReachSoFar >= lastIndex;
+class Solution {
+    public boolean advanceBySoluton(int[] arr) {
+        int maxSoFar = 0;
+        int lastIndex = arr.length - 1;
+
+        for(int i = 0; i < arr.length; i++) {
+            maxSoFar = Math.max(maxSoFar, i + arr[i]);
+        }
+
+        return maxSoFar >= lastIndex;
     }
 }
