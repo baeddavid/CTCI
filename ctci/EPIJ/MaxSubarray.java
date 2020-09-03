29a2a7ef3891590e4f121dec6fa2e2716c4c6e18
David Bae <baeddavid@gmail.com>
2020-09-02 21:32:12 -0500

    add recursive solution for max subarray

--- ctci/EPIJ/MaxSubarray.java
+++ ctci/EPIJ/MaxSubarray.java
@@ -21,7 +21,7 @@ class MaxSubarray {
         for(int i = 0; i < arr.length; i++) {
             maxSoFar += arr[i];
             if(maxSoFar > max) {
-                max = maxSoFar;
+                    max = maxSoFar;
             }
             if(maxSoFar < 0) {
                 maxSoFar = 0;
@@ -30,8 +30,36 @@ class MaxSubarray {
         return max;
     }
 
+    // Divide and Conquer
+    public static int divideConquer(int[] arr) {
+        return divideHelper(arr, 0, arr.length - 1);
+    }
+
+    private static int findCrossingSum(int[] arr, int left, int med, int right) {
+        int leftSum = 0, rightSum = 0, sum = 0;
+        for(int i = med; i >= 0; i--) {
+            sum += arr[i];
+            if(sum > leftSum) leftSum = sum;
+        }
+
+        sum = 0;
+        for(int i = med + 1; i <= right; i++) {
+            sum += arr[i];
+            if(sum > rightSum) rightSum = sum;
+        }
+
+        return leftSum + rightSum;
+    }
+
+    private static int divideHelper(int[] arr, int left, int right) {
+        if(left == right) return arr[left];
+        int med = (left + right) / 2;
+        return Math.max(divideHelper(arr, left, med),
+                        Math.max(divideHelper(arr, med + 1 , right), findCrossingSum(arr, left, med, right)));
+    }
+
     public static void main(String[] args) {
         int[] A = new int[] {2,1,-3,4,-1,2,1,-5,4};
-        System.out.print(kadane(A));
+        System.out.print(divideConquer(A));
     }
 }
