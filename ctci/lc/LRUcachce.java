69009f8eee3451418d78c04711f6047fe0db0982
David Bae <baeddavid@gmail.com>
2020-09-04 01:28:39 -0500

    add passing solution for leetcode

--- ctci/lc/LRUcachce.java
+++ ctci/lc/LRUcachce.java
@@ -1,152 +1,117 @@
 import java.util.HashMap;
 
 class LRUCache {
-    // Attributes
-    public LRULinkedList list;
-    public HashMap <Integer, LRUNode> map;
+    public final LinkedList list;
+    public final HashMap<Integer, Node> map;
     public int capacity;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        list = new LRULinkedList();
+        list = new LinkedList();
         map = new HashMap<>();
     }
 
     public int get(int key) {
-        LRUNode retrievedNode;
-        if(map.containsKey(key)) {
-            retrievedNode = map.get(key);
-        } else {
-            return -1;
-        }
-        if(list.getSize() != 1) {
-            list.delete(retrievedNode);
-            list.insert(retrievedNode.data.key, retrievedNode.data.val);
-        }
-        return retrievedNode.data.val;
+        Node node = map.get(key);
+        if(node == null) { return -1; }
+        list.moveToHead(node);
+        return node.val;
     }
 
     public void put(int key, int val) {
-        int currentSize = list.getSize();
-        if(map.containsKey(key)) {
-            System.out.print("here");
-            list.ammend(map.get(key), val);
-            list.delete(map.get(key));
-            list.insert(map.get(key).data.key, map.get(key).data.val);
-            return;
-        } if(currentSize == capacity) {
-            LRUNode node = list.deleteTail();
-            map.remove(node.data.key);
+        Node node = map.get(key);
+        if(node == null) {
+            Node newNode = new Node(key, val);
+            map.put(key, newNode);
+            list.insert(newNode);
+
+            if(list.size() > capacity) {
+                Node deleted = list.delete(list.getTail());
+                map.remove(deleted.key);
+            }
+        } else {
+            node.val = val;
+            list.moveToHead(node);
         }
-        map.put(key, new LRUNode(key, val));
-        list.insert(key, val);
     }
 
     public void print() {
         list.print();
     }
+}
 
-    private boolean isFull() {
-        return list.getSize() == capacity;
+class Node {
+    public Node next;
+    public Node prev;
+    public int val;
+    public int key;
+
+    public Node(int key, int val) {
+        this.val = val;
+        this.key = key;
+        next = null;
+        prev = null;
     }
 }
 
-class LRULinkedList {
-    private LRUNode head;
-    private LRUNode tail;
-    private int size;
+class LinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
 
-    public LRULinkedList() {
-        head = null;
-        tail = null;
+    public LinkedList() {
         size = 0;
+        head = new Node(-1, -1);
+        head.prev = null;
+
+        tail = new Node(-1, -1);
+        tail.next = null;
+
+        head.next = tail;
+        tail.next = head;
     }
 
-    public void ammend(LRUNode node, int val) {
-        node.data.val = val;
-        System.out.println("I changed a value");
-    }
-
-    // Insert Method - Inserts from the front. We are building list backwards.
-    // TODO take a node
-    public void insert(int key, int val) {
-        LRUNode newNode = new LRUNode(key, val);
-        newNode.next = head;
-        newNode.prev = null;
-        if(head == null) { tail = newNode; }
-        if(head != null) { head.prev = newNode; }
-        head = newNode;
+    // Insert Method inserts a node at the head
+    public void insert(Node node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
         size++;
     }
 
-    // Delete Method - Removes a node from the list in constant time
-    // TODO make it return a node
-    public void delete(LRUNode node) {
-        if(node.next == null) {
-            deleteTail();
-        } else {
-            node.data = node.next.data;
-            node.next.next.prev = node;
-            node.next = node.next.next;
-            size--;
-        }
+    public Node delete(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        Node deleted = node;
+
+        prev.next = next;
+        next.prev = prev;
+        size--;
+
+        return deleted;
     }
 
-    // Delete Tail Method - Removes the tail from the list
-    public LRUNode deleteTail() {
-        if(tail.next == null && tail.prev == null) {
-            LRUNode node = tail;
-            head = null;
-            tail = null;
-            return node;
-        } else {
-        LRUNode deletedNode = tail;
-        tail = tail.prev;
-        tail.next.prev = null;
-        tail.next = null;
-        size--;
-        return deletedNode;
-        }
+    public void moveToHead(Node node) {
+        delete(node);
+        insert(node);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Node getTail() {
+        return tail.prev;
     }
 
     public void print() {
-        LRUNode current = head;
-        while(current != null) {
-            System.out.print("(" + current.data.key + "," + current.data.val + ")");
+        Node current = head.next;
+        while(current != tail) {
+            System.out.print("(" + current.val + ")");
             current = current.next;
         }
         System.out.println();
-    }
-
-    public LRUNode getTail() {
-        return tail;
-    }
-
-    // Size Check Method for list
-    public int getSize() {
-        return size;
-    }
-}
-
-class LRUNode {
-    public LRUNode next;
-    public LRUNode prev;
-    public LRUPair data;
-
-    public LRUNode(int key, int val) {
-        next = null;
-        prev = null;
-        data = new LRUPair(key, val);
-    }
-}
-
-class LRUPair {
-    public int key;
-    public int val;
-
-    public LRUPair(int key, int val) {
-        this.key = key;
-        this.val = val;
     }
 }
 
@@ -154,13 +119,13 @@ public class LRUcachce {
     public static void main(String[] args) {
         LRUCache cache = new LRUCache(2);
         cache.put(2,1);
-        cache.put(1,1);
+        cache.put(2,2);
         cache.print();
-        cache.put(2,3);
+        print(cache.get(2));
+        cache.put(1,1);
         cache.print();
         cache.put(4,1);
         cache.print();
-        print(cache.get(1));
         print(cache.get(2));
     }
 
