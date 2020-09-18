846ea83a5bd527e6346cb4461c9dd80b56997c0d
David Bae <baeddavid@gmail.com>
2020-09-17 23:51:13 -0500

    corrected bug where the stack was not being properly updated

--- ctci/lc/ZigZagLevelOrderTraversal.java
+++ ctci/lc/ZigZagLevelOrderTraversal.java
@@ -1,37 +1,35 @@
-class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> traversals = new ArrayList<>();
-        if(root = null) {
-            return traversals;
-        }
-        Stack<TreeNode> stack = new Stack<>();
-        boolean isRight = false;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
 
-        while(!stack.isEmpty()) {
-            Stack<TreeNode> temp = new Stack<>();
-            List<Integer> currentTraversal = new ArrayList<>();
-            while(!stack.isEmpty()) {
-                TreeNode current = stack.pop();
-                currentTraversal.add(current.val);
-                if(isRight) {
-                    if(current.left != null) {
-                        temp.push(current.left);
-                    }
-                    if(current.right != null) {
-                        temp.push(current.right);
-                    }
-                } else {
-                    if(current.right != null) {
-                        temp.push(current.right);
-                    }
-                    if(current.left != null) {
-                        temp.push(current.left);
-                    }
-                }
-           }
-            traversals.add(currentTraversal);
-            isRight = !isRight;
-        }
-        return traversals;
-    }
-}
+class Solution {
+     List<List<Integer>> zigzaglevelOrder(TreeNode root) {
+         List<List<Integer>> traversal = new ArrayList<>();
+         if(root == null) { return traversal };
+
+         Stack<TreeNode> stack = new Stack<>();
+         stack.add(root);
+         boolean isGoingRight = true;
+
+         while(!stack.isEmpty()) {
+             Stack<TreeNode> temp = new Stack<>();
+             List<Integer> cTraversal = new ArrayList<>();
+             while(!stack.isEmpty()) {
+                 TreeNode current = stack.pop();
+                 cTraversal.add(current.val);
+                 if(isGoingRight) {
+                     if(current.left!= null) { temp.add(current.left); }
+                     if(current.right != null) { temp.add(current.right); }
+                     isGoingRight = !isGoingRight;
+                 } else {
+                     if(current.right != null) { temp.add(current.right); }
+                     if(current.left != null){ temp.add(current.left); }
+                     isGoingRight = !isGoingRight;
+                 }
+             }
+             stack = temp;
+             traversal.add(cTraversal);
+         }
+         return traversal;
+     }
+ }
