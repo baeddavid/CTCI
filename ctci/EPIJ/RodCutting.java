017fa1847ca933ec01077d01d30a3204ac8b54dc
David Bae <baeddavid@gmail.com>
2020-09-30 16:14:07 -0500

    add solution for rod cutting

--- ctci/EPIJ/RodCutting.java
+++ ctci/EPIJ/RodCutting.java
@@ -8,4 +8,22 @@ public class RodCutting {
         }
         return max;
     }
+
+    // Memoized Solution
+    public static int rodCutting(int[] prices, int length) {
+        int[] cache = new int[length + 1];
+        Arrays.fill(cache, Integer.MIN_VALUE);
+        return rodHelper(prices, length, cache);
+    }
+
+    private static int rodHelper(int[] prices, int length, int[] cache) {
+        if(cache[length] >= 0) { return cache[length]; }
+        if(length == 0) { return 0; }
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < length; i++) {
+            max = Math.max(max, prices[i] + rodHelper(prices, length - i - 1, cacher));
+        }
+        return max;
+    }
 }
