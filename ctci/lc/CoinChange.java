29c0d6dbabe7c5592a9ac504f232aca9ef7c53ce
David Bae <baeddavid@gmail.com>
2020-10-05 21:06:37 -0500

    update solution for coin change

--- ctci/lc/CoinChange.java
+++ ctci/lc/CoinChange.java
@@ -27,9 +27,4 @@ public class CoinChange {
         }
         return coins[amount];
     }
-
-    public static void main(String[] args) {
-        int[] coins = new int[] { 1, 2, 5 };
-        System.out.println(coinChange(coins, 11));
-    }
 }
