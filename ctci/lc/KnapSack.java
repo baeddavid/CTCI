b60a9628b779b0ece6deef82019b9283b06a64ac
David Bae <baeddavid@gmail.com>
2020-10-19 14:38:58 -0500

    update the solution:

--- ctci/lc/KnapSack.java
+++ ctci/lc/KnapSack.java
@@ -18,9 +18,14 @@ class KnapSack {
     // DP Solution
     public static int knapSack(int bagWeight, int[] weights, int[] values, int n) {
         int[][] cache = new int[n + 1][bagWeight + 1];
+        // Iterate through all items
         for(int i = 0; i <= n; i++) {
+            // Iterate through all possible bag combinations.
             for(int w = 0; w <= bagWeight, w++) {
+                // If i or w is 0, we assign the current position in the 2d array to 0
                 if(i == 0 || w == 0) { cache[i][w] = 0; }
+                // if the current weight of the item is less than or equal to the weight of the bag
+                // we take the max of either including the item or excluding the item.
                 else if(weights[i - 1] <= bagWeight) {
                     cache[i][w] = max(
                                       values[i - 1] + cache[i - 1][bagWeight - weights[i - 1]],
