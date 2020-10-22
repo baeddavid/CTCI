a7a16e0161f65a8cf0e00e90656c040c72223690
David Bae <baeddavid@gmail.com>
2020-10-22 16:28:41 -0500

    add solution that uses ascii character counts

--- ctci/Chapter1/QuestionTwo.java
+++ ctci/Chapter1/QuestionTwo.java
@@ -14,7 +14,29 @@ public class QuestionTwo {
         return Arrays.equals(string1, string2);
     }
 
+    public static boolean checkPermutationCharacterCount(String s1, String s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        int[] chars = new int[128];
+
+        for(int i = 0; i < s1.length(); i++) {
+            chars[s1.charAt(i)]++;
+        }
+
+        for(int i = 0; i < s2.length(); i++) {
+            chars[s1.charAt(i)]--;
+        }
+
+        for(int count : chars) {
+            if(count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println(checkPermutationSort("This is a test to see if it works", "works This is a test to see if it"));
+        System.out.println(checkPermutationCharacterCount("This is a test to see if it works", "works This is a test to see if it"));
     }
 }
