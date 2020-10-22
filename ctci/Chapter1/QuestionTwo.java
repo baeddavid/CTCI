104b660d4a099e419ec488882446fc3556971f44
David Bae <baeddavid@gmail.com>
2020-10-22 16:24:12 -0500

    add basic solution for checking if two strings are permutations of each other

---
+++ ctci/Chapter1/QuestionTwo.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+// Given two strings write a method to decide if one is a permutation of the other
+public class QuestionTwo {
+    // The question again is a bit ambiguous. We have no indication whether the strings are capitalized, have punctuation or white space.
+    // The safe method here would be to convert both strings to a common rule.
+    public static boolean checkPermutationSort(String s1, String s2) {
+        char[] string1 = s1.toLowerCase().toCharArray();
+        char[] string2 = s2.toLowerCase().toCharArray();
+
+        Arrays.sort(string1);
+        Arrays.sort(string2);
+
+        return Arrays.equals(string1, string2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPermutationSort("This is a test to see if it works", "works This is a test to see if it"));
+    }
+}
