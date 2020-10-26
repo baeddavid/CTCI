7841f71b28af966b49fdafb9372298b2e1a01f84
David Bae <baeddavid@gmail.com>
2020-10-25 20:09:48 -0500

    add solution for question four

---
+++ ctci/Chapter2/QuestionFour.java
@@ -0,0 +1,60 @@
+// Write code to partition a linked list around a value x, such that all nodes lessthan x come before all nodes gresater than or eqwual to x.
+// If x is contained within the list, the values of x only need to be after the elements less than x. The partition element x can appear anywhere in the "right partition"
+// it does not need to appear between the left and right partitions
+
+public class QuestionFour {
+    public static Node partition(Node head, int x) {
+        Node lessThan = new Node(-1);
+        Node greaterThan = new Node(-1);
+        Node lessThanHead = lessThan, greaterThanHead = greaterThan;
+
+        Node current = head;
+        while(current != null) {
+            Node next = current.next;
+            if(current.data < x) {
+                lessThan.next = current;
+                lessThan = lessThan.next;
+            } else {
+                greaterThan.next = current;
+                greaterThan = greaterThan.next;
+            }
+            current.next = null;
+            current = next;
+        }
+        lessThan.next = greaterThanHead.next;
+        return lessThanHead.next;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(3);
+        Node b = new Node(5);
+        Node c = new Node(8);
+        Node d = new Node(5);
+        Node e = new Node(10);
+        Node f = new Node(2);
+        Node g = new Node(1);
+
+        a.next = b;
+        b.next = c;
+        c.next= d;
+        d.next = e;
+        e.next = f;
+        f.next = g;
+
+        Node answer = partition(a, 5);
+        while(answer != null) {
+            System.out.println(answer.data + " ");
+            answer = answer.next;
+        }
+    }
+}
+
+class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
