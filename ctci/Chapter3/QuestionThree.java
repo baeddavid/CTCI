2413d5a580ab3ba77adca29f3c810ffaf74c7e3a
David Bae <baeddavid@gmail.com>
2020-10-28 20:02:21 -0500

    add problem

---
+++ ctci/Chapter3/QuestionThree.java
@@ -0,0 +1,8 @@
+// Imagine a literal stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we sould likely start a new stack when the
+// previous stack exceeds some threshold. Implement a data structure SetOfStacks that mimics this. SetOfStacks should be composed of several stacks and
+// should create a new stack once the previous one exceeds capacity.
+
+
+public class QuestionThree {
+
+}
