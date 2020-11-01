4293d07241093bb7cbd0de2a382f081cd6fcb291
David Bae <baeddavid@gmail.com>
2020-11-01 17:53:08 -0600

    add solution

---
+++ ctci/Chapter3/QuestionSix.java
@@ -0,0 +1,59 @@
+// An animal shelter, which holds only dogs and cats, operates on a strictly "first in, first out" basis. People must adopt either the "oldest" of all animals at the shelter,
+// or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of that type). They cannot select which specific animal they would like .
+// Create the data structures to maintain this system and implement operations such as enque, dequeueAny, dequeueDog, and dequeueCat. You may use the built-in LinkedList data structure.
+
+// Sounds like queue implementation
+
+import java.util.LinkedList;
+
+public class QuestionSix {
+    public static void main(String[] args) {
+        AnimalShelter animalShelter = new AnimalShelter();
+        animalShelter.enque("dog");
+        animalShelter.enque("cat");
+        animalShetler.enque("dog");
+    }
+}
+
+class AnimalShelter {
+    public LinkedList<Animal> list;
+
+    public AnimalShelter() {  }
+
+    public void enque(String breed, String name) {
+        Animal newAnimal = new Animal(breed, name);
+        list.add(newAnimal);
+    }
+
+    public Animal dequeAny() {
+        return list.remove();
+    }
+
+    public Animal dequeDog() {
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).breed == "dog") {
+                return list.remove(i);
+            }
+        }
+        return null;
+    }
+
+    public Animal dequeCat() {
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).breed = "cat") {
+                return list.remove(i);
+            }
+        }
+        return null;
+    }
+}
+
+class Animal {
+    public String breed;
+    public String name;
+
+    public Animal(String breed, String name) {
+        this.name = name;
+        this.breed = breed;
+    }
+}
