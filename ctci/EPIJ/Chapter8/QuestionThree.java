f00d3e1cbd7252131cbac7375b7c6671d7cb6be0
David Bae <baeddavid@gmail.com>
2020-11-09 21:05:43 -0600

    add solution for magic index

---
+++ ctci/EPIJ/Chapter8/QuestionThree.java
@@ -0,0 +1,15 @@
+public class QuestionThree {
+    public static int magicIndex(int[] A, int left, int right) {
+        if(left > right) { return -1; }
+        int med = (left + right) / 2;
+
+        if(A[med] == med) { return med; }
+        else if(A[med] < med) { return magicIndex(A, med + 1, right); }
+        else { return magicIndex(A, left, med - 1); }
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[] { -7, -3, 1, 3, 7, 12 };
+        System.out.println(magicIndex(A, 0, A.length));
+    }
+}
