8f815cbad2f07e243d8cdf7f302cd06518473520
David Bae <baeddavid@gmail.com>
2020-11-21 00:58:24 -0600

    add complex question two solution

---
+++ ctci/EPIJ/Chapter11/QuestionTwo.java
@@ -0,0 +1,20 @@
+public class QuestionTwo {
+    public static int index(int[] A, int left, int right) {
+        if(left > right) {
+            return -1;
+        }
+        int med = (left + right) / 2;
+        if(A[med] == med) {
+            return med;
+        } else if(A[med] > med) {
+            return index(A, left, med - 1);
+        } else {
+            return index(A, med + 1, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[] { -4, -2, 0, 3, 5 };
+        System.out.print(index(A, 0, A.length - 1));
+    }
+}
