ce97c2b66a09aab9aba0c86e606d171dc067269f
David Bae <baeddavid@gmail.com>
2020-11-24 18:22:14 -0600

    add solution for validating bst"

---
+++ MonthlyLC/ValidateTree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValidVST(TreeNode root) {
+        return helper(root, null, null)
+    }
+
+    private boolean helper(TreeNode node, Integer left, Integer right) {
+        if(node == null) { return true; }
+        if(left != null && node.val <= left) { return false; }
+        if(right != null && node.val >= right) { return false; }
+
+        if(!helper(node.left, left, node.val)) { return false; }
+        if(!helper(node.right, node.val, right)) { return false; }
+        return true;
+    }
+}
