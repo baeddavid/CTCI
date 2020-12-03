5b8aef79f752d0319cc5e2f01c2043b2c75b3d71
David Bae <baeddavid@gmail.com>
2020-12-03 01:20:42 -0600

    add solution for populate right pointers

---
+++ MonthlyLC/PopulateRightPointers.java
@@ -0,0 +1,26 @@
+class Solution {
+    // Breadth first traversal where each level is stored in an array
+    public Node connext(Node root) {
+        if(root == null)  { return root; }
+        Queue<Node> queue = new LinkedList<>();
+        Node current = root;
+        queue.add(current);
+
+        while(!queue.isEmpty()) {
+            Queue<Node> temp = new LinkedList<>();
+            List<Node> list = new ArrayList<>();
+            while(!queue.isEmpty()) {
+                current = queue.remove();
+                if(current.left != null) { temp.add(current.left); }
+                if(current.right != null) { temp.add(current.right); }
+                list.add(current);
+            }
+            for(int i = 0; i < list.size() - 1; i++) {
+                list.get(i).next = list.get(i + 1);
+            }
+            list.get(list.size() - 1).next = null;
+            queue = temp;
+        }
+        return root;
+    }
+}
